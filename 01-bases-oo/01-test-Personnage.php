fdbacfc1b6041d05b46318ddaca3d919721ca015
24manu <manuel.mouzelard@hotmail.com>
2022-02-17 16:08:22 +0100

    Personnage suite

--- 01-bases-oo/01-test-Personnage.php
+++ 01-bases-oo/01-test-Personnage.php
@@ -6,11 +6,6 @@ $moi = new Personnage();
 
 $toi = new Personnage();
 
-$a = 5;
-$b = $a;
-
-$lui = $toi;
-
 echo 'moi est une instance de Personnage (1er instance) :';
 
 var_dump($moi);
@@ -18,3 +13,9 @@ var_dump($moi);
 echo 'toi est une instance de Personnage (2er instance) :';
 
 var_dump($toi);
+
+echo "<h4>Affichage d'attributs publics</h4>";
+echo '<p>$moi->nom affiche ' . $moi->nom . '</p>';
+echo '<p>$toi->nom affiche ' . $toi->nom . '</p>';
+
+echo "<h4>L'Affichage d'attributs non publics n'est pas possible de la même manière</h4>";
--- 01-bases-oo/Personnage.php
+++ 01-bases-oo/Personnage.php
@@ -1,4 +1,10 @@
 <?php
 class Personnage
 {
+    private $force = 50;
+    private $endurance = 100;
+    private $degats;
+
+    public $nom = "Anonyme";
+    public $experience;
 }
