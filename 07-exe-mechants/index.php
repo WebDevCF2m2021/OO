906fd13d2d1d9e720ba2e8b5edd80a0aeadc2c9e
24manu <manuel.mouzelard@hotmail.com>
2022-03-17 08:40:23 +0100

    exe01

--- 07-exe-mechants/index.php
+++ 07-exe-mechants/index.php
@@ -1,7 +1,8 @@
 <?php
 // chargement des dépendances
 require_once "config.php";
-// ...
+require_once "Mechant.php";
+require_once "MechantManager.php";
 
 // tentative de connexion à notre DB
 try {
@@ -11,7 +12,7 @@ try {
 }
 
 // Instanciation de notre manager avec la connexion PDO
-// $persoManager = new PersonnageManager($db);
+$mechantManager = new MechantManager($db);
 
 
 
@@ -29,26 +30,41 @@ try {
 <body>
 <?php
 $cahit = new Mechant([
-        "lulu"=>"lala",
-        "youpiCaBoum"=>"yes",
-        "slup"=>25.25,
-        "vie"=>5,
-        25=>"jkhjih",
-        "nom"=>"pause",
+    "nameMechant"=>"cahit",
+        "forceMechant"=>1,
+        "experienceMechant"=>2,
+        "statusMechant"=>3
 ]);
 
 var_dump($cahit);
-
+$balek = $mechantManager->SelectAllMechant();
+var_dump($balek);
 
 
 ?><hr>
-<?php
 
-?>
-<h3>Insertion depuis un formulaire dans Mechant</h3>
+<h3>Insertion depuis un formulaire</h3>
 <form action="" method="post" name="balek">
 
+    <input name='nameMechant' type="text" placeholder="votre nom"><br>
+    <input name='forceMechant' type="number" placeholder=forceMechant><br>
+    <input name='experienceMechant' type="number" placeholder='experienceMechant'><br>
+    <input name='statusMechant' type="number" placeholder='StatusMechant'><br>
+    <input type="submit" value="créer le personnage">
 </form>
+<?php
+
+// si on a envoyé le formulaire
+if(!empty($_POST)){
+    // instanciation des valeurs passée par le formulaire
+    $insert = new Mechant($_POST);
+    var_dump($insert);
+    // insertion dans la DB
+    echo $mechantManager->insertMechant($insert);
+
+}
+
+?>
 
 <h3>Affichez tous les méchants !</h3>
 </body>
